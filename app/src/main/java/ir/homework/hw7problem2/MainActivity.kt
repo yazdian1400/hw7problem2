39a7a454be37831493d41af4dd7ab140a468aa01
Maedeh <yazdian.oul@gmail.com>
2022-02-23 16:25:29 +0330

    declare setQuestionsAndAnswers function

--- app/src/main/java/ir/homework/hw7problem2/MainActivity.kt
+++ app/src/main/java/ir/homework/hw7problem2/MainActivity.kt
@@ -6,10 +6,37 @@ import ir.homework.hw7problem2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding : ActivityMainBinding
+    val questionList = mutableListOf<String>()
+    val answerList = mutableListOf<Boolean>()
+    val userAnswerList = mutableListOf<UserAnswer>()
+    val cheatList = mutableListOf<Boolean>()
+    var numOfQuestion = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setTitle("GeoQuiz")
+        setQuestionsAndAnswers()
+        binding.tvQuestion.text = questionList[numOfQuestion]
+        binding.btnPrev.isClickable = false
     }
+
+    fun setQuestionsAndAnswers(){
+        questionList.add(getString(R.string.question1))
+        answerList.add(stringToBoolean(getString(R.string.answer1)))
+        questionList.add(getString(R.string.question2))
+        answerList.add(stringToBoolean(getString(R.string.answer2)))
+
+    }
+    fun stringToBoolean(str: String): Boolean{
+        return str == "true"
+    }
+}
+
+enum class UserAnswer{
+    NOANSWER,
+    TRUE,
+    FALSE
 }
