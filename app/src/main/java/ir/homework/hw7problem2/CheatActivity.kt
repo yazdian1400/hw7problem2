f04c1e6b31fabf257d297550021e501aaf0fa950
Maedeh <yazdian.oul@gmail.com>
2022-02-23 20:26:23 +0330

    design the CheatActivity and set view binding for it

--- app/src/main/java/ir/homework/hw7problem2/CheatActivity.kt
+++ app/src/main/java/ir/homework/hw7problem2/CheatActivity.kt
@@ -2,10 +2,14 @@ package ir.homework.hw7problem2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ir.homework.hw7problem2.databinding.ActivityCheatBinding
 
 class CheatActivity : AppCompatActivity() {
+    lateinit var binding: ActivityCheatBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cheat)
+        binding = ActivityCheatBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
